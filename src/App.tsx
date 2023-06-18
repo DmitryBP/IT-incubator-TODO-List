2d6aa5dfdbf3b6037f26cf78a535277e65d86b65
Dmitry_home <79811924115@yandex.ru>
2023-06-18 15:23:23 +0300

    Урок 5 не до конца

--- src/App.tsx
+++ src/App.tsx
@@ -2,43 +2,78 @@ import React, { useState } from 'react';
 
 import './App.css';
 import TodoList from './TodoList/TodoList';
-
-const initialTasks = [
-  { id: 1, content: 'HTML&CSS', isChecked: false },
-  { id: 2, content: 'JS', isChecked: true },
-  { id: 3, content: 'React', isChecked: false },
-];
+import { v1 } from 'uuid';
 
 export type filterValueType = 'all' | 'active' | 'complited';
+type TodoListType = {
+  id: string;
+  title: string;
+  filter: filterValueType;
+};
 
 function App() {
-  const [tasks, setTaskList] = useState(initialTasks);
-  const [filter, setFilter] = useState<filterValueType>('all');
+  const [todoLists, setTodoLists] = useState<Array<TodoListType>>([
+    { id: v1(), title: 'Whot to do', filter: 'active' },
+    { id: v1(), title: 'Whot to lern', filter: 'complited' },
+  ]);
+  const [tasks, setTaskList] = useState([
+    { id: v1(), content: 'HTML&CSS', isChecked: false },
+    { id: v1(), content: 'JS', isChecked: false },
+    { id: v1(), content: 'React', isChecked: false },
+  ]);
 
-  const filtredTasks =
-    filter === 'active'
-      ? tasks.filter((t) => t.isChecked === false)
-      : filter === 'complited'
-      ? tasks.filter((t) => t.isChecked === true)
-      : tasks;
-
-  const cheingeFilter = (filterValue: filterValueType) => {
-    setFilter(filterValue);
+  const cheingeFilter = (filterValue: filterValueType, todoListId: string) => {
+    let todoList = todoLists.find((t)=>t.id===todoListId)
+    if(todoList){
+      todoList.filter = filterValue
+      setTodoLists([...todoLists])
+    }
   };
 
-  const removeTask = (id: number) => {
+  const removeTask = (id: string) => {
     const removedTaskList = tasks.filter((t) => t.id !== id);
     setTaskList(removedTaskList);
   };
 
+  const addTask = (inputValue: string) => {
+    const addObj = { id: v1(), content: inputValue, isChecked: false };
+
+    const newTasks = [addObj, ...tasks];
+    setTaskList(newTasks);
+  };
+
+  const changeTaskStatus = (id: string) => {
+    const targetTask = tasks.find((t) => t.id === id);
+    if (targetTask) {
+      targetTask.isChecked ? (targetTask.isChecked = false) : (targetTask.isChecked = true);
+      setTaskList([...tasks]);
+    }
+  };
+
   return (
     <div className="App">
-      <TodoList
-        title="What to do"
-        tasks={filtredTasks}
-        removeTask={removeTask}
-        cheingeFilter={cheingeFilter}
-      />
+      {todoLists.map((t) => {
+        const filtredTasks =
+          t.filter === 'active'
+            ? tasks.filter((t) => t.isChecked === false)
+            : t.filter === 'complited'
+            ? tasks.filter((t) => t.isChecked === true)
+            : tasks;
+
+        return (
+          <TodoList
+            key={t.id}
+            id={t.id}
+            title={t.title}
+            tasks={filtredTasks}
+            removeTask={removeTask}
+            cheingeFilter={cheingeFilter}
+            addTask={addTask}
+            changeTaskStatus={changeTaskStatus}
+            filter={t.filter}
+          />
+        );
+      })}
       ;
     </div>
   );
--- src/TodoList/TodoList.tsx
+++ src/TodoList/TodoList.tsx
@@ -1,37 +1,98 @@
 import { filterValueType } from '../App';
 import s from './todoList.module.scss';
+import { ChangeEvent, KeyboardEvent, useState } from 'react';
 
-type todoItemsListType = {
-  id: number;
-  content: string;
-  isChecked: boolean;
-};
+export type todoItemsListType = { id: string; content: string; isChecked: boolean };
 
 type propsType = {
+  id: string;
   title: string;
   tasks: Array<todoItemsListType>;
-  removeTask: (id: number) => void;
-  cheingeFilter: (filterValue: filterValueType) => void;
+  removeTask: (id: string) => void;
+  cheingeFilter: (filterValue: filterValueType, todoListId: string ) => void;
+  addTask: (inputValue: string) => void;
+  changeTaskStatus: (id: string) => void;
+  filter: string;
 };
 
-export default function TodoList({ title, tasks, removeTask, cheingeFilter }: propsType) {
+export default function TodoList({
+  id,
+  title,
+  tasks,
+  removeTask,
+  cheingeFilter,
+  addTask,
+  changeTaskStatus,
+  filter,
+}: propsType) {
+  const [inputValue, setInputValue] = useState('');
+  const [errorStatus, setErrorStatus] = useState(false);
+
+  const cleanAddedTask = () => {
+    addTask(inputValue);
+    setInputValue('');
+  };
+
+  const onCheingInput = (e: ChangeEvent<HTMLInputElement>) => {
+    const textInInput = e.currentTarget.value;
+    setInputValue(textInInput);
+  };
+  const onKeyUp = (e: KeyboardEvent<HTMLInputElement>) => {
+    setErrorStatus(false);
+    if (e.keyCode === 13 && inputValue.trim() !== '') {
+      cleanAddedTask();
+    } else if (e.keyCode === 13 && inputValue.trim() === '') {
+      setErrorStatus(true);
+      setInputValue('');
+    }
+  };
+  const onClickAddTask = () => {
+    if (inputValue.trim() !== '') {
+      cleanAddedTask();
+    } else setErrorStatus(true);
+    setInputValue('');
+  };
+  const onFilterAll = () => cheingeFilter('all', id);
+  const onFilterActive = () => cheingeFilter('active', id);
+  const onFilterComplited = () => cheingeFilter('complited', id);
   return (
     <div className={s.todoListContainer}>
       <h3>{title}</h3>
-      <input />
-      <button>+</button>
+
+      <div>
+        <input
+          value={inputValue}
+          onChange={onCheingInput}
+          onKeyUp={onKeyUp}
+          className={errorStatus === true ? s.error : undefined}
+        />
+        <button onClick={onClickAddTask}>+</button>
+      </div>
+      {errorStatus === true ? <div className={s.errorText}>Поле обязательно для заполнения</div> : null}
       <ul>
-        {tasks.map((t) => (
-          <li>
-            <input type="checkbox" checked={t.isChecked} />
-            {t.content}
-            <button onClick={() => removeTask(t.id)}>x</button>
-          </li>
-        ))}
+        {tasks.map((t) => {
+          const onRemove = () => removeTask(t.id);
+          return (
+            <li key={t.id}>
+              <div>
+                <input type="checkbox" onChange={() => changeTaskStatus(t.id)} checked={t.isChecked} />
+                <span className={t.isChecked ? s.doneTask : undefined}>{t.content}</span>
+              </div>
+              <button onClick={onRemove} className={s.delBtn}></button>
+            </li>
+          );
+        })}
       </ul>
-      <button onClick={() => cheingeFilter('all')}>Все</button>
-      <button onClick={() => cheingeFilter('active')}>Активные</button>
-      <button onClick={() => cheingeFilter('complited')}>Выполненные</button>
+      
+      <button onClick={onFilterAll} className={filter === 'all' ? s.activeFilterBtn : undefined}>
+        Все
+      </button>
+      <button onClick={onFilterActive} className={filter === 'active' ? s.activeFilterBtn : undefined}>
+        Активные
+      </button>
+      <button onClick={onFilterComplited} className={filter === 'complited' ? s.activeFilterBtn : undefined}>
+        Выполненные
+      </button>
     </div>
   );
 }
